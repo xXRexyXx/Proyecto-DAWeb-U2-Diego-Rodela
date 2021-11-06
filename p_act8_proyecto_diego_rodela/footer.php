d4e065dc4d5a85a31276e8c673814caaf46c91c1
xXRexyXx <91646041+xXRexyXx@users.noreply.github.com>
2021-11-06 04:06:50 -0700

    Delete p_act8_proyecto_diego_rodela directory

--- p_act8_proyecto_diego_rodela/footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-		<div class="wrapp">
-            <p> No lista 41 </p>
-            <p> // SUBMÓDULO 2
-                Desarrolla aplicaciones Web con conexión a bases de datos</p>
-            <p>Repositorio en git hub </p>
-		</div>
-	</footer>
-</body>
